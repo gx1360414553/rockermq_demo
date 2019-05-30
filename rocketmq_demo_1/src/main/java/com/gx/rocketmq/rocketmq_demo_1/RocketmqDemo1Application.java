65e1fe815e0a20d7fac515e4c37f33270be29c73
gx1360414553 <199695gfl>
2019-05-30 09:37:31 +0800

    Initial commit

---
+++ rocketmq_demo_1/src/main/java/com/gx/rocketmq/rocketmq_demo_1/RocketmqDemo1Application.java
@@ -0,0 +1,13 @@
+package com.gx.rocketmq.rocketmq_demo_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqDemo1Application.class, args);
+    }
+
+}
